7b8b74a49f3560ccc084575f038d480c45f1f5f6
Kewin Erichsen <Kewin Erichsen>
2017-03-21 17:16:59 +0100

    ProductionPane layout

--- src/prjct/ProductionPane.java
+++ src/prjct/ProductionPane.java
@@ -18,4 +18,56 @@ public class ProductionPane extends BasicPane {
 	public ProductionPane(Database db) {
 		super(new Database());
 	}
+
+	/**
+	 * Create the left top panel. Contains tools for determining the number
+	 * of tables to be used in the seating.
+	 *
+	 * @return the left top panel.
+	 */
+	public JComponent createLeftTopPanel() {
+		JPanel panel = new JPanel();
+		Box mainBox = new Box(BoxLayout.Y_AXIS);
+		Box box = new Box(BoxLayout.X_AXIS);
+		
+		JComboBox dropDown = new JComboBox();
+		JSpinner nbrOfPallets = customSpinner(new SpinnerNumberModel(1,1,500,1), 50, 25);
+		JButton produce = customButton("Produce",null, 100, 25);
+
+		box.add(nbrOfPallets);
+		box.add(Box.createHorizontalStrut(200));
+		box.add(produce);
+
+		mainBox.add(Box.createVerticalStrut(50));
+		mainBox.add(dropDown);
+		mainBox.add(Box.createVerticalStrut(10));
+		mainBox.add(box);
+
+		panel.add(mainBox);
+		panel.setBorder(new LineBorder(Color.BLACK));
+
+		return panel;
+	}
+
+	/**
+	 * Create the left middle panel.
+	 *
+	 * @return the left middle panel.
+	 */
+	public JComponent createLeftMiddlePanel() {
+		JPanel panel = new JPanel();
+
+		return panel;
+	}
+
+	/**
+	 * Create the left bottom panel.
+	 *
+	 * @return the left bottom panel.
+	 */
+	public JComponent createLeftBottomPanel() {
+		JPanel panel = new JPanel();
+
+		return panel;
+	}
 }
